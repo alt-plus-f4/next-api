fefd8a1175e99fb66833f8ae9265cfbf5e362139
alt-plus-f4 <valentin.assenov@abv.bg>
2024-01-13 02:04:26 +0200

    rmrfd

--- src/app/api/rmrf/route.ts
+++ src/app/api/rmrf/route.ts
@@ -5,5 +5,8 @@ export async function DELETE() {
     const deletedItems = await db.item.deleteMany();
     const deletedCases = await db.case.deleteMany();
 
+    await db.$executeRaw`ALTER TABLE \`Item\` AUTO_INCREMENT = 1`;
+    await db.$executeRaw`ALTER TABLE \`Case\` AUTO_INCREMENT = 1`;
+
     return NextResponse.json({ item: deletedItems, case: deletedCases }, { status: 200 })
 }
