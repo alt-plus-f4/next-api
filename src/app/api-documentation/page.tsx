445016ff6697d0f8cee93164647758670d4842b2
alt-plus-f4 <valentin.assenov@abv.bg>
2024-01-16 17:18:32 +0200

    Added api-documentation

---
+++ src/app/api-documentation/page.tsx
@@ -0,0 +1,9 @@
+// import React from "react";
+// import SwaggerUI from "swagger-ui-react";
+// import "swagger-ui-react/swagger-ui.css";
+
+// const SwaggerDocs = () => {
+//   return <SwaggerUI url="/swagger.yaml" />;
+// };
+
+// export default SwaggerDocs;
