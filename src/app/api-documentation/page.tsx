9e32b0c8b2551a4bb4769d0ed5a1809d2dc59830
alt-plus-f4 <valentin.assenov@abv.bg>
2024-01-25 19:55:54 +0200

    removed swagger page for now

--- src/app/api-documentation/page.tsx
+++
@@ -1,9 +0,0 @@
-// import React from "react";
-// import SwaggerUI from "swagger-ui-react";
-// import "swagger-ui-react/swagger-ui.css";
-
-// const SwaggerDocs = () => {
-//   return <SwaggerUI url="/swagger.yaml" />;
-// };
-
-// export default SwaggerDocs;
